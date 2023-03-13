dfa5f21d3bb73b3337b3ef07de275ab13b6e4e80
Kay Gosho <ketsume0211@gmail.com>
2023-03-14 02:53:44 +0900

    add basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useRef } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const STORAGE_KEY = 'mini-editor:value'
+
+export function App() {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null!)
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.value = localStorage.getItem(STORAGE_KEY) || ''
+    }
+  }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (textAreaRef.current) {
+        localStorage.setItem(STORAGE_KEY, textAreaRef.current.value)
+      }
+    }, 500)
+    return () => clearInterval(timer)
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="w-screen h-screen">
+      <h1 className="text-3xl font-bold underline">Mini Editor</h1>
+      <textarea
+        ref={textAreaRef}
+        autoFocus
+        className="w-full h-full p-1"
+      ></textarea>
     </div>
   )
 }
-
-export default App
